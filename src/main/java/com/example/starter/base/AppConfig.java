92cd16bc54b8b993242bfaf3ea08cdcfde3930dd
Marco Collovati <mcollovati@gmail.com>
2025-10-16 16:47:30 +0200

    use aura theme

--- src/main/java/com/example/starter/base/AppConfig.java
+++ src/main/java/com/example/starter/base/AppConfig.java
@@ -1,8 +1,9 @@
 package com.example.starter.base;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 
-@Theme("starter-theme")
+@StyleSheet("styles.css")
 public class AppConfig implements AppShellConfigurator {
 }
